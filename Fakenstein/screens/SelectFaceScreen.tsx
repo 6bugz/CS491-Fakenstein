03f1380216592c036c0b71328afd4aeb4e6d77d0
elifKurtay <elif.kurtay00@gmail.com>
2022-04-09 19:30:50 +0300

    svg demo

--- Fakenstein/screens/SelectFaceScreen.tsx
+++ Fakenstein/screens/SelectFaceScreen.tsx
@@ -4,6 +4,11 @@ import { StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Colors from '../constants/Colors';
 import { Text, View } from '../components/Themed';
 
+import Svg, {
+  Circle,
+  Rect
+} from 'react-native-svg';
+
 
 export default function SelectFaceScreen({route, navigation}) {
     const [image, setImage] = useState(null);
@@ -16,7 +21,27 @@ export default function SelectFaceScreen({route, navigation}) {
 
     return (
     <View style={styles.container}>
-      {!!image && <Image source={{ uri: image.uri }} style={{ height: '90%', width: '90%'}} />}
+      {!!image &&
+        <Svg height="50%" width="50%" viewBox="0 0 100 100">
+          <Image source={{ uri: image.uri }} style={{ height: '90%', width: '90%'}}/>
+          <Circle
+            cx="50"
+            cy="50"
+            r="45"
+            stroke="blue"
+            strokeWidth="2.5"
+            fill="green"
+          />
+          <Rect
+            x="15"
+            y="15"
+            width="70"
+            height="70"
+            stroke="red"
+            strokeWidth="2"
+            fill="yellow"
+          />
+        </Svg>}
     </View>
     );
 }
